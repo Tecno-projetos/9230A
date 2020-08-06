c6f1d37e280f4aa75013c49ea7727de6edc7d735
maycon pereti lutz <maycon.lutz@hotmail.com>
2020-08-06 14:25:38 -0300

    Cadastro Produtos finalizando

--- 9230A V00 - PI/DataBase/SqlFunctionsProdutos.cs
+++ 9230A V00 - PI/DataBase/SqlFunctionsProdutos.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _9230A_V00___PI.DataBase
 {
     public class SqlFunctionsProdutos
     {
+
+        public static void ExistTable()
+        {
+
+            DataTable Data_Produtos = new DataTable();
+
+            if (Utilidades.VariaveisGlobais.DB_Connected_GS)
+            {
+                try
+                {
+                    string CommandString_Produtos = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Produtos';";
+
+                    dynamic Call_Produtos = SqlGlobalFuctions.ReturnCall(Utilidades.VariaveisGlobais.Connection_DB_Produtos_GS);
+                    dynamic Adapter_Produtos = SqlGlobalFuctions.ReturnAdapter(CommandString_Produtos, Utilidades.VariaveisGlobais.Connection_DB_Produtos_GS);
+
+                    Adapter_Produtos.Fill(Utilidades.VariaveisGlobais.Connection_DB_Produtos_GS);
+                }
+                catch (Exception ex)
+                {
+                    Utilidades.VariaveisGlobais.Window_Buffer_Diagnostic.List_Error = ex.ToString();
+                }
+
+                if (!(Data_Produtos.Rows.Count > 0))
+                {
+                    Create_Table_Produtos();
+                }
+            }
+        }
 
         private static void Create_Table_Produtos()
         {
@@ -15,7 +45,6 @@ namespace _9230A_V00___PI.DataBase
             {
                 try
                 {
-                    //==== BUT BEFORE REMEMBER TO CHECK IF THERE IS A JA TABLE WITH THE NAME YOU WANT TO ====
                     string CommandString = "CREATE TABLE Produtos (" +
                         "Codigo string not null primary key," +
                         "Descricao nvarchar(200)," +
@@ -37,6 +66,44 @@ namespace _9230A_V00___PI.DataBase
                 }
             }
         }
+
+        public static string IntoDate_Table_Produtos(string codigo, string descricao, double densidade, string tipoProduto, string observacao)
+        {
+            string ret = "";
+
+            if (Utilidades.VariaveisGlobais.DB_Connected_GS)
+            {
+                try
+                {
+
+                    dynamic Call = SqlGlobalFuctions.ReturnCall(Utilidades.VariaveisGlobais.Connection_DB_Produtos_GS);
+
+                    string query = "INSERT into Produtos (Codigo, Descricao, Densidade, TipoProduto, Observacao) VALUES (@Codigo, @Descricao, @Densidade, @TipoProduto, @Observacao)";
+                    dynamic Command = SqlGlobalFuctions.ReturnCommand(query, Call);
+                    Command.Parameters.AddWithValue("@Codigo", codigo);
+                    Command.Parameters.AddWithValue("@Descricao", descricao);
+                    Command.Parameters.AddWithValue("@Densidade", densidade);
+                    Command.Parameters.AddWithValue("@TipoProduto", tipoProduto);
+                    Command.Parameters.AddWithValue("@TipoProduto", observacao);
+                    Call.Open();
+                    Command.ExecuteNonQuery();
+                    Call.Close();
+                    ret = "ok";
+                }
+                catch (Exception ex)
+                {
+                    Utilidades.VariaveisGlobais.Window_Buffer_Diagnostic.List_Error = ex.ToString();
+                    ret = "erro";
+                }
+
+                return ret;
+            }
+            else
+            {
+                return ret;
+            }
+        }
+
 
     }
 }
--- 9230A V00 - PI/Telas Fluxo/Receitas/CadastroProdutos.xaml.cs
+++ 9230A V00 - PI/Telas Fluxo/Receitas/CadastroProdutos.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _9230A_V00___PI.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,124 @@ namespace _9230A_V00___PI.Telas_Fluxo.Receitas
     /// </summary>
     public partial class CadastroProdutos : UserControl
     {
+        Utilidades.messageBox inputDialog;
+
+        long codigoProduto = 0;
+        float densidade = 0.0f;
+        string tipoProduto = "";
+
         public CadastroProdutos()
         {
             InitializeComponent();
+            limpaCampos();
+        }
+
+        private void lbMateriaPrima_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (lbMateriaPrima.IsSelected)
+            {
+                pckMateriaPrima.Visibility = Visibility.Visible;
+                pckComplemento.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void lbComplemento_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (lbComplemento.IsSelected)
+            {
+                pckMateriaPrima.Visibility = Visibility.Hidden;
+                pckComplemento.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            limpaCampos();
+        }
+
+        private void limpaCampos()
+        {
+
+            //limpa campos
+            txtCod.Text = "";
+            txtDesc.Text = "";
+            txtDensidade.Text = "";
+            txtObs.Text = "";
+
+            lbMateriaPrima.IsSelected = true;
+            pckComplemento.Visibility = Visibility.Hidden;
+            pckMateriaPrima.Visibility = Visibility.Visible;
+        }
+
+        private void openKeyboard(object sender, MouseButtonEventArgs e)
+        {
+            Teclados.keyboard.openKeyboard();
+        }
+
+        private void btCadastrarProduto_Click(object sender, RoutedEventArgs e)
+        {
+            if (VariaveisGlobais.DB_Connected_GS)
+            {
+                if (String.IsNullOrEmpty(txtCod.Text) || String.IsNullOrEmpty(txtDesc.Text) || String.IsNullOrEmpty(txtDensidade.Text))
+                {
+                    inputDialog = new Utilidades.messageBox("Campos Vazios", "Por favor verifique se todos os campos necessários estão preenchidos", MaterialDesignThemes.Wpf.PackIconKind.Error, "OK", "Fechar");
+
+                    inputDialog.ShowDialog();
+                }
+                else
+                {
+                    if (lbComplemento.IsSelected || lbMateriaPrima.IsSelected)
+                    {
+                        if (long.TryParse(txtCod.Text, out codigoProduto))
+                        {
+                            if (float.TryParse(txtDensidade.Text.Replace('.', ','), out densidade))
+                            {
+                                if (lbComplemento.IsSelected)
+                                {
+                                    tipoProduto = "Complemento";
+                                }
+                                else if (lbMateriaPrima.IsSelected)
+                                {
+                                    tipoProduto = "Matéria Prima";
+                                }
+
+
+                                DataBase.SqlFunctionsProdutos.IntoDate_Table_Produtos(codigoProduto.ToString(), txtDesc.Text, densidade, tipoProduto, txtObs.Text);
+
+
+                                inputDialog = new Utilidades.messageBox("Cadastro", "Produto: " + txtDesc.Text + " Cadastrado com Sucesso!", MaterialDesignThemes.Wpf.PackIconKind.Plus, "OK", "Fechar");
+
+                                inputDialog.ShowDialog();
+
+                            }
+                            else
+                            {
+                                inputDialog = new Utilidades.messageBox("Densidade", "Por favor verifique se contém somente número real na densidade", MaterialDesignThemes.Wpf.PackIconKind.Error, "OK", "Fechar");
+
+                                inputDialog.ShowDialog();
+                            }
+                        }
+                        else
+                        {
+                            inputDialog = new Utilidades.messageBox("Código Produto", "Por favor verifique se contém somente números inteiros no Código Produto", MaterialDesignThemes.Wpf.PackIconKind.Error, "OK", "Fechar");
+
+                            inputDialog.ShowDialog();
+                        }
+                    }
+                    else
+                    {
+                        inputDialog = new Utilidades.messageBox("Tipo de Produto", "Por favor selecione o tipo de Produto", MaterialDesignThemes.Wpf.PackIconKind.Error, "OK", "Fechar");
+
+                        inputDialog.ShowDialog();
+                    }
+                }
+            }
+            else
+            {
+                inputDialog = new Utilidades.messageBox("Sem conexão com Banco de Dados", "Por favor verifique a conexão com o Banco de Dados", MaterialDesignThemes.Wpf.PackIconKind.DatabaseRefresh, "OK", "Fechar");
+
+                inputDialog.ShowDialog();
+            }
         }
     }
 }
